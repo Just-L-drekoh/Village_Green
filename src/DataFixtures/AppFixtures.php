da5968f35b3ea924c33e6e7ccf2a2032dffcc17a
logan <loloherbo.drekoh@gmail.com>
2024-11-21 16:42:39 +0100

    update fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,17 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Utilisateurs
 
-        $manager->flush();
+        $user = new User();
+        
     }
 }
