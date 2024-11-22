167bce873a6a3160c74bcec371ac1a57a3e538ce
logan <loloherbo.drekoh@gmail.com>
2024-11-22 11:10:25 +0100

    update profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,22 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use App\Repository\AddressRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfileController extends AbstractController
 {
     
-    public function profile(): Response
+    public function profile(UserRepository $userRepository, AddressRepository $addressRepository): Response
     {
         $user = $this->getUser();
-
+        
         if (!$user) {
             return $this->redirectToRoute('app_login');
         }
 
-
+       
         return $this->render('profile/profile.html.twig', [
             'controller_name' => 'ProfileController',
             'user' => $user
